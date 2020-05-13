cb29e9ab7b0fe39ae7f936c148148a055ecab0f7
Oleg Polyakov <olegpolyakov@outlook.com>
2020-05-13 16:44:48 +0300

    Update metrics

--- includes/google-analytics.php
+++ includes/google-analytics.php
@@ -1,9 +1,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-150655844-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-166286877-1"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'UA-150655844-1');
+    gtag('config', 'UA-166286877-1');
 </script>
--- includes/yandex-metrika.php
+++ includes/yandex-metrika.php
@@ -1,6 +1,6 @@
 <!-- Yandex.Metrika -->
 <script type="text/javascript" >
-    window.YANDEX_METRIKA_COUNTER = 55877350;
+    window.YANDEX_METRIKA_COUNTER = 62741278;
 
     (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
     m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
@@ -13,5 +13,5 @@
         webvisor:true
     });
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/55877350" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/62741278" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika -->
