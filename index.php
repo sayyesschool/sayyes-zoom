c787b2cb321b2bf5c2532e41f1c7f500acec2dce
Oleg Polyakov <olegpolyakov@outlook.com>
2020-08-13 16:12:08 +0300

    Replace Vimeo with YT

--- index.php
+++ index.php
@@ -299,7 +299,7 @@
                 </div>
 
                 <div class="box video">
-                    <iframe src="https://player.vimeo.com/video/414731269" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
+                    <iframe src="https://www.youtube-nocookie.com/embed/jSfeDjvLxlI?controls=0&rel=0&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </section>
 
@@ -485,7 +485,7 @@
                 <p class="subtitle">Посмотрите не просто отзывы наших студентов, а то, как они <strong>говорят на английском</strong>:</p>
 
                 <div class="box video">
-                    <iframe src="https://player.vimeo.com/video/319753562" webkitallowfullscreen="" mozallowfullscreen="" allowfullscreen=""></iframe>
+                    <iframe src="https://www.youtube-nocookie.com/embed/PnizC-6xOB8?controls=0&rel=0&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </section>
 
